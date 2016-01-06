0789ea88f11b357a6312ca156fdb7baf906878b7
lilongsen <limingxing_aqgy@sina.com>
2016-01-06 22:16:45 +0800

    two
    two

---
+++ BaseJava/src/test/java/com/test/MyTest.java
@@ -0,0 +1,24 @@
+/*package com.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.base.service.UserService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/config/spring.xml")
+public class MyTest {
+	@Autowired
+	private UserService userService;
+	@Test
+	public void test() {
+		System.out.println(userService);
+	}
+
+}
+*/
